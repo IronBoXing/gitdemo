8d63d0414ddd602b4c24d7f9743e0401557aab19
IronBob <IronBob@Ali.com>
2022-02-05 08:32:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ironbob
+ * @create 2022-02-05 8:28
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
